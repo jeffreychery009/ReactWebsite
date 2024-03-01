541d9aa3d80251b57941fbaebedfbf4efc63e245
Jeffrey Chery <jeffchery009@gmail.com>
2024-03-01 15:09:01 -0500

    Created a Constant folder where we add predefined constants with information

---
+++ src/Constants/index.ts
@@ -0,0 +1,19 @@
+export interface NavLink {
+  id: string;
+  title: string;
+}
+
+export const navLinks: NavLink[] = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
