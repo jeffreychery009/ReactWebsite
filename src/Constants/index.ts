6d663166a07ca45c6093dd706dab8c7a8e2815f5
Jeffrey Chery <157074250+jeffreychery009@users.noreply.github.com>
2024-03-22 01:13:11 -0400

    Update index.ts

--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -21,6 +21,10 @@ export const navLinks: NavLink[] = [
     id: "contact",
     title: "Contact",
   },
+  {
+    id: "experience",
+    title: "Experience",
+  },
 ];
 
 export interface description {
